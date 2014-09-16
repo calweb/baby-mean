a899231f45460584774b727866c1f14984817243
Calvin Webster <calweb@gmail.com>
2014-09-15 23:35:59 -0400

    because hoisting and better readibility

--- public/posts/postsService.js
+++ public/posts/postsService.js
@@ -5,38 +5,6 @@
         .module('posts')
         .factory('postsService', ['$http', '$rootScope', function ($http, $rootScope) {
 
-
-
-            var baseUrl = "api/collections/demotiy";
-
-            var getPosts = function () {
-
-                return $http.get(baseUrl);
-            };
-
-            var getPost = function (postId) {
-                return $http.get(baseUrl + "/" + postId);
-            };
-
-            var createPost = function (newPost) {
-                $http.post(baseUrl, newPost).then(function (res) {
-                    $rootScope.$broadcast("post:added");
-                })
-            };
-
-            var editPost = function (post) {
-                $http.put(baseUrl + "/" + post._id, post).then(function (res) {
-                    $rootScope.$broadcast("post:updated");
-                })
-
-            };
-
-            var deletePost = function (postId) {
-                $http.delete(baseUrl + "/" + postId).then(function (res) {
-                    $rootScope.$broadcast("post:deleted");
-                })
-            };
-
             // public service methods
             return {
                 getPosts: getPosts,
@@ -45,6 +13,36 @@
                 editPost: editPost,
                 deletePost: deletePost
             };
+
+            function getPosts() {
+
+                return $http.get("api/collections/demotiy");
+            };
+
+            function getPost(postId) {
+                return $http.get("api/collections/demotiy/" + postId);
+            };
+
+            function createPost(newPost) {
+                $http.post("api/collections/demotiy", newPost).then(function (res) {
+                    $rootScope.$broadcast("post:added");
+                })
+            };
+
+            function editPost(post) {
+                $http.put("api/collections/demotiy/" + post._id, post).then(function (res) {
+                    $rootScope.$broadcast("post:updated");
+                })
+
+            };
+
+            function deletePost(postId) {
+                $http.delete("api/collections/demotiy/" + postId).then(function (res) {
+                    $rootScope.$broadcast("post:deleted");
+                })
+            };
+
+
 
         }]);
 })();
